57af9c369b436922df781ec2586cc15a506e0968
MuadDAtr <int.stron@protonmail.com>
2023-02-10 23:59:23 +0100

    init screen

--- calc.py
+++ calc.py
@@ -7,7 +7,18 @@ def initializeWindow():
     root.title('Calc')
     return root
 
+def initializeScreen(root):
+    screen = [tk.Label(root, width = 60, bg = '#545454' ) for i in range(3)]
+
+    for i in range(len(scren)):
+        screen[i].grid(row - 1, column = 0)
+    return screen
+
 if __name__ == '__main__':
 
     root = initializeWindow()
+    screen = initializeScreen()
+
     root.mainloop()
+
+
