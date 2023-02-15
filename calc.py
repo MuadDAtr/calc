ccf70ef183af32f879f4d78279093209cdb1380d
MuadDAtr <int.stron@protonmail.com>
2023-02-15 23:43:17 +0100

    buttons

--- calc.py
+++ calc.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+symbols = ['7','8','9', '/', 'C', 
+            '4', '5', '6', '*', '(',
+            '1', '2', '3', '+', ')',
+            ',', '0', '%', '-', '=']
+
 def initializeWindow():
     root = tk.Tk()
 
@@ -11,20 +16,20 @@ def initializeScreen(root):
     screen = [tk.Label(root, width = 60, bg = '#545454', text = 'test', anchor = 'center', borderwidth = 3 ) for i in range(3)]
 
     for i in range(len(screen)):
-        screen[i].grid(row = i, column = 0, ipadx = 10, ipady = 10)
+        screen[i].grid(row = i, columnspan = 5, ipadx = 10, ipady = 10)
     return screen
 
 def initializeDataField(root, screen):
     data_field = tk.Entry(root)
-    data_field.grid(row = len(screen), column = 0, ipadx = 100, ipady = 5)
+    data_field.grid(row = len(screen), columnspan = 5, ipadx = 100, ipady = 5)
 
     info = tk.Label(root, width = 60, bg = 'green', text = 'info', anchor = 'center', borderwidth = 2)
-    info.grid(row = len(screen) + 1, column = 0, ipady = 15, ipadx = 1)
+    info.grid(row = len(screen) + 1, columnspan = 5, ipady = 15, ipadx = 1)
 
     return data_field
 
 def initializeButtons(root, screen):
-    
+
 
 
 if __name__ == '__main__':
